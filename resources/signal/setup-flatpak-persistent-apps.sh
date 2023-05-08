6690cde19f993252aa45f260b859b08843ae60ee
dutu <dutu@protonmail.com>
2023-05-08 16:15:02 +0300

    add flatpak assets

--- resources/signal/setup-flatpak-persistent-apps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-persistence_dir=/live/persistence/TailsData_unlocked
-mkdir -p /home/amnesia/.local/share
-
-if ! file /home/amnesia/.local/share/flatpak | grep -q 'symbolic link'; then
-  rm -rf --one-file-system /home/amnesia/.local/share/flatpak
-  ln -s $persistence_dir/flatpak /home/amnesia/.local/share/flatpak
-fi
